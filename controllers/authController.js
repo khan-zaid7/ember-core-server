d7771da746055ca089716663785a7bb84cf052a2
m-dkhan <mdzaidkhan628@gmail.com>
2025-06-10 18:51:12 -0400

    feat(SCRUM-21): Build user registration API with express server and routing

---
+++ controllers/authController.js
@@ -0,0 +1,40 @@
+// controllers/authController.js
+import admin from '../config/firebaseAdmin.js';
+import { createUserDoc } from '../models/userModel.js';
+
+export const registerUser = async (req, res) => {
+    console.log('working');
+  const { email, password, name, role, phone_number } = req.body;
+
+  if (!email || !password || !name || !role) {
+    return res.status(400).json({ error: 'email, password, name, and role are required' });
+  }
+
+  try {
+    const userRecord = await admin.auth().createUser({
+      email,
+      password,
+      displayName: name,
+    });
+
+    await admin.auth().setCustomUserClaims(userRecord.uid, { role });
+
+    await createUserDoc(userRecord.uid, {
+      name,
+      email,
+      role,
+      phone_number,
+    });
+
+    return res.status(201).json({
+      message: 'User registered successfully',
+      user_id: userRecord.uid,
+      email: userRecord.email,
+      role,
+      name: userRecord.displayName,
+    });
+  } catch (error) {
+    console.error('[Register Error]', error);
+    return res.status(400).json({ error: error.message });
+  }
+};
