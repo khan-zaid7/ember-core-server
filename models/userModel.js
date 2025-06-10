735b7a4c4cdd55df99019ac71e541793846e3ffc
m-dkhan <mdzaidkhan628@gmail.com>
2025-06-10 18:14:03 -0400

    feat(SCRUM-22): Create user document in Firestore with role and secure password

---
+++ models/userModel.js
@@ -0,0 +1,20 @@
+// models/userModel.js
+import admin from '../config/firebaseAdmin.js';
+import { FieldValue } from 'firebase-admin/firestore';
+
+const db = admin.firestore();
+const usersCollection = db.collection('users');
+
+export const createUserDoc = async (uid, data) => {
+  return await usersCollection.doc(uid).set({
+    user_id: uid,
+    name: data.name,
+    email: data.email,
+    phone_number: data.phone_number || null,
+    role: data.role,
+    reset_token: null,
+    token_expire: null,
+    created_at: FieldValue.serverTimestamp(),
+    updated_at: FieldValue.serverTimestamp(),
+  });
+};
